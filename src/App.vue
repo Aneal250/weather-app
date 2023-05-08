616dcb619575d03ad48a4ff9acbb8ebe9ca66dc1
Anaele emekah <62405019+Aneal250@users.noreply.github.com>
2023-05-08 07:46:45 +0100

    background color Update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="bg-primary min-h-screen grid place-items-center">
+  <div class="bg min-h-screen grid place-items-center">
     <RouterView />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.bg {
+  background-image: linear-gradient(180deg, #2937b3, #187ac8);
+}
+</style>
